77bc03ed2c540009383dded722e907f4550ace8e
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-03-27 18:14:45 -0400

    Add pagination

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -2,8 +2,23 @@ import prisma from '@/lib/prisma';
 import { NextResponse, NextRequest } from 'next/server'
 
 export async function GET(request: Request) { 
+  
+  const { searchParams } = new URL(request.url)
+  const take = Number(searchParams.get('take') ?? '10');
+  const skip = Number(searchParams.get('skip') ?? '0');
 
-  const todos = await prisma.todo.findMany();
+  if ( isNaN(take) ) {
+    return NextResponse.json({ message: 'Invalid take parameter' }, { status: 400 });
+  }
+
+  if ( isNaN(skip) ) {
+    return NextResponse.json({ message: 'Invalid skip parameter' }, { status: 400 });
+  }
+
+  const todos = await prisma.todo.findMany({
+    take: take,
+    skip: skip,
+  });
 
   return NextResponse.json( todos );
 }
