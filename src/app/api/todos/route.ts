f08b5562ba3719a25e35dffd7199df58552f5ae5
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-04-05 17:47:12 -0400

    Implement a POST endpoint to add a new item to the database

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -22,3 +22,12 @@ export async function GET(request: Request) {
 
   return NextResponse.json( todos );
 }
+
+export async function POST(request: Request) {
+
+  const body = await request.json();
+
+  const todo = await prisma.todo.create({ data: body })
+
+  return NextResponse.json(todo);
+}
