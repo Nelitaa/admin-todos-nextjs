74a26f8a0c798b57662be1ea147bd8e1e9c8726e
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-04-06 17:40:07 -0400

    Add a validation schema using Yup for posts

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/lib/prisma';
 import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup';
 
 export async function GET(request: Request) { 
   
@@ -23,11 +24,21 @@ export async function GET(request: Request) {
   return NextResponse.json( todos );
 }
 
+const postSchema = yup.object({
+  description: yup.string().required(),
+  completed: yup.boolean().optional().default(false),
+})
+
 export async function POST(request: Request) {
 
-  const body = await request.json();
+  try {
+    const { description, completed } = await postSchema.validate ( await request.json() );
 
-  const todo = await prisma.todo.create({ data: body })
+    const todo = await prisma.todo.create({ data: { description, completed } });
+  
+    return NextResponse.json(todo);
 
-  return NextResponse.json(todo);
+  } catch (error) {
+    return NextResponse.json( error, { status: 400 });
+  }
 }
