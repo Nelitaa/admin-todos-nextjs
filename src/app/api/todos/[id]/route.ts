95f7850d7970af9e9ee25fa7e5f94e1f775bfeb7
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-04-06 17:58:41 -0400

    Implement a PUT endpoint to update an item

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -18,3 +18,22 @@ export async function GET(request: Request, { params }: Segments ) {
 
   return NextResponse.json(todo)
 }
+
+export async function PUT(request: Request, { params }: Segments ) { 
+
+  const { id } = params;
+  const todo = await prisma.todo.findFirst({ where: { id } });
+  
+  if (!todo){
+    return NextResponse.json({ message: `To do with id: ${ id } not found` }, { status: 404 });
+  }
+
+  const body = await request.json();
+
+  const updatedTodo = await prisma.todo.update({ 
+    where: { id }, 
+    data: { ...body } 
+  });
+
+  return NextResponse.json(todo)
+}
