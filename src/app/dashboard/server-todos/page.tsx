8e287cee76a36b13a63a1ca457298aeb4b4e42ed
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-04-09 17:51:11 -0400

    Create server-todos page

---
+++ src/app/dashboard/server-todos/page.tsx
@@ -0,0 +1,23 @@
+import prisma from "@/lib/prisma";
+import { NewTodo, TodosGrid } from "@/todos";
+
+export const metadata = {
+  title: 'List of tasks to do',
+  description: 'REST Todos page',
+};
+
+export default async function ServerTodosPage() {
+
+  const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' } });
+
+  return (
+    <>
+      <span className="text-3xl mb-10">Server Actions</span>
+      <div className="w-full px-3 mx-5 mb-5">
+        <NewTodo />
+      </div>
+      
+      <TodosGrid todos={ todos } />
+    </>
+  );
+}
