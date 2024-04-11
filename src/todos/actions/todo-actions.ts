e920836579b5235c87c52d892eaf9566a3f452d2
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-04-11 18:29:45 -0400

    Implement useOptimistic Hook

--- src/todos/actions/todo-actions.ts
+++ src/todos/actions/todo-actions.ts
@@ -3,7 +3,18 @@
 import prisma from '@/lib/prisma';
 import { revalidatePath } from 'next/cache';
 
+export const sleep = async( seconds: number = 0 ) => {
+  return new Promise( resolve => {
+    setTimeout( () => {
+      resolve(true);
+     }, seconds * 1000 );
+  });
+}
+
 export const toggleTodo = async( id: string, completed: boolean ): Promise<Todo> => {
+  
+  await sleep(3);
+  
   const todo = await prisma.todo.findFirst({ where: { id } });
   
   if ( !todo ) {
--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useOptimistic } from 'react';
 import { Todo } from "@prisma/client";
 import styles from './TodoItem.module.css';
 import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
@@ -10,26 +11,41 @@ interface Props {
 }
 
 export const TodoItem = ({ todo, toggleTodo }: Props) => {
+
+  const [ todoOptimistic, toggleTodoOptimistic ] = useOptimistic( 
+    todo,
+    (state, newCompleteValue: boolean ) => ({ ...state, completed: newCompleteValue })
+  );
+
+  const onToggleTodo = async () => {
+    try {
+      toggleTodoOptimistic( !todoOptimistic.completed );
+      await toggleTodo( todoOptimistic.id, !todoOptimistic.completed );
+    } catch (error) {
+      toggleTodoOptimistic( !todoOptimistic.completed );
+    }
+  }
+
   return (
-    <div className={ todo.completed ? styles.todoDone : styles.todoPending }>
+    <div className={ todoOptimistic.completed ? styles.todoDone : styles.todoPending }>
       <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
 
         <div 
-          onClick={ () => toggleTodo( todo.id, !todo.completed ) }
+          onClick={ onToggleTodo }
         className={`
           flex p-2 rounded-md cursor-pointer
           hover:bg-opacity-60
-          ${ todo.completed ? 'bg-blue-100' : 'bg-red-100' }
+          ${ todoOptimistic.completed ? 'bg-blue-100' : 'bg-red-100' }
         `}>
           {
-            todo.completed
+            todoOptimistic.completed
               ? <IoCheckboxOutline size={30} />
               : <IoSquareOutline size={30} />
           }
         </div>
 
         <div className="text-center sm:text-left">
-          { todo.description }
+          { todoOptimistic.description }
         </div>
 
       </div>
