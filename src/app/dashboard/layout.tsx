addd7460f8ab957bd7a548e68edce6b30c0f239f
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-04-12 15:16:08 -0400

    Fix main layout styles

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -16,7 +16,7 @@ export default function DashboardLayout({
         <TopMenu />
 
         {/* TODO: Contenido en el Layout.tsx */}
-        <div className="px-6 pt-6">
+        <div className="px-6 pt-6 bg-white p-2 m-2 rounded">
           {children}
         </div>
       </div>
