f5c631fd61c75efdfb876e9688acf912eb420193
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-04-12 17:04:47 -0400

    Set cookie - server side

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,4 +1,5 @@
 import { TabBar } from "@/components";
+import { cookies } from 'next/headers'
 
 export const metadata = {
   title: "Cookies",
@@ -6,11 +7,15 @@ export const metadata = {
 };
 
 export default function CookiesPage() {
+
+  const cookieStore = cookies()
+  const cookieTab = cookieStore.get('selectedTab')?.value ?? '1';
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
       <div className="flex flex-col">
         <span className="text-3xl">Tabs</span>
-        <TabBar />
+        <TabBar currentTab={ +cookieTab }/>
       </div>
     </div>
   );
