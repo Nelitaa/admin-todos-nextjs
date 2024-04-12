ae36873c7e507e2bdb4d1376ee5253229f65f707
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-04-12 11:20:51 -0400

    Fix dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,9 @@
+import { WidgetItem } from "@/components";
 
 export default function DashboardPage() {
   return (
-    <div>
-      <h1>Dashboard Page</h1>
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      <WidgetItem />
     </div>
   );
 }
--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -2,7 +2,6 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation';
-import { CiBookmarkCheck } from 'react-icons/ci'
 
 interface Props {
   icon: React.ReactNode;
