171a1edb4f6b3d665c747baa95fc4cbc80e00c80
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-04-12 16:03:23 -0400

    Create TabBar component

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,8 +1,17 @@
+import { TabBar } from "@/components";
+
+export const metadata = {
+  title: "Cookies",
+  description: "Cookies page",
+};
 
 export default function CookiesPage() {
   return (
-    <div>
-      <h1>Cookies Page</h1>
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
+      <div className="flex flex-col">
+        <span className="text-3xl">Tabs</span>
+        <TabBar />
+      </div>
     </div>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
-
 export { Sidebar } from './Sidebar';
 export { SidebarItem } from './SidebarItem';
 export { TopMenu } from './TopMenu';
 export { WidgetItem } from './WidgetItem';
+export { TabBar } from './TabBar';
