f9c968c03731786335d6a59d97f9e0f394a28d79
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-04-13 15:41:22 -0400

    Show all products

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,9 +1,14 @@
 import { ProductCard } from "@/products";
+import { products } from "@/products/data/products";
 
 export default function ProductsPage() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
-      <ProductCard />
+      {
+        products.map((product) => (
+          <ProductCard key={product.id} {...product } />
+        ))
+      }
     </div>
   );
 }
--- src/products/components/ProductCard.tsx
+++ src/products/components/ProductCard.tsx
@@ -1,11 +1,20 @@
 // https://tailwindcomponents.com/component/e-commerce-product-card
 
-import Image from "next/image"
-import { IoAddCircleOutline, IoTrashOutline } from "react-icons/io5"
+import Image from "next/image";
+import { IoAddCircleOutline, IoTrashOutline } from "react-icons/io5";
+import { Star } from "./Star";
 
-export const ProductCard = () => {
+interface Props {
+  id    : string;
+  name  : string;
+  price : number;
+  rating: number;
+  image : string;
+}
+
+export const ProductCard = ({ id, name, price, rating, image }:Props) => {
   return (
-    <div className="bg-white shadow rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-100">
+    <div className="shadow rounded-lg max-w-sm bg-gray-800 border-gray-100">
       
       {/* Product Image */}
       <div className="p-2">
@@ -13,73 +22,41 @@ export const ProductCard = () => {
             width={500}
             height={500}
             className="rounded" 
-            src="/images/products/1623735-00-A_0_2000.jpg" 
+            src={ image }
             alt="product image" />
       </div>
       
       {/* Title */}
       <div className="px-5 pb-5">
         <a href="#">
-          <h3 className="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Apple Watch Series 7
-            GPS, Aluminium Case, Starlight Sport</h3>
+          <h3 className="font-semibold text-xl tracking-tight text-white">{ name }</h3>
         </a>
         <div className="flex items-center mt-2.5 mb-5">
-          
 
           {/* Stars */}
-          <svg className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
-            </path>
-          </svg>
-
-          <svg className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
-            </path>
-          </svg>
-
-          <svg className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
-            </path>
-          </svg>
-
-          <svg className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
-            </path>
-          </svg>
-
-          <svg className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
-            </path>
-          </svg>
+          {
+            Array(rating).fill(0).map((x,i) => (
+              <Star key={i} />
+            ))
+          }
 
           {/* Rating Number */}
-          <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-            5.0
+          <span className="text-xs font-semibold mr-2 px-2.5 py-0.5 rounded bg-blue-200 text-blue-800 ml-3">
+            { rating.toFixed(2) }
           </span>
         </div>
 
-
         {/* Price and Add to Cart */}
         <div className="flex items-center justify-between">
-          <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+          <span className="text-3xl text-white font-bold">$ { price }</span>
           
           <div className="flex">
             <button
-              className="text-white mr-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              className="text-white mr-2 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
                 <IoAddCircleOutline size={25} />
             </button>
             <button
-              className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+              className="text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-red-600 hover:bg-red-700 focus:ring-red-800">
                 <IoTrashOutline size={20} />
             </button>
           </div>
