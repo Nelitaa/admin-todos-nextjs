0069172edd29be6ffa5e2317ded22c1398c7edd1
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-04-13 17:49:33 -0400

    Update top menu

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -2,20 +2,22 @@ import { cookies } from 'next/headers';
 import React from 'react'
 import { CiChat1, CiMenuBurger, CiSearch, CiShoppingBasket } from 'react-icons/ci'
 
+
+const getTotalCount = (cart: { [id: string]: number }):number => {
+  let items =0;
+
+  Object.values( cart ).forEach( value => {
+    items += value as number;
+  })
+  return items;
+}
+
 export const TopMenu = () => {
 
   const cookieStore = cookies();
   const cart =JSON.parse( cookieStore.get('cart')?.value ?? '{}' );
 
-  const getTotalCount = () => {
-    let items =0;
-
-    Object.values( cart ).forEach( value => {
-      items += value as number;
-    })
-
-    return items;
-  }
+  const totalItems = getTotalCount( cart );
 
   return (
     <div className="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
@@ -43,7 +45,9 @@ export const TopMenu = () => {
             <CiChat1 size={25} />
           </button>
           <button className="p-2 flex items-center justify-center h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
-            <span className='text-sm mr-2 text-blue-800 font-bold'>{ getTotalCount() }</span>
+            {
+              ( totalItems > 0 ) && (<span className='text-sm mr-2 text-blue-800 font-bold'>{ totalItems }</span>)
+            }
             <CiShoppingBasket size={25} />
           </button>
 
