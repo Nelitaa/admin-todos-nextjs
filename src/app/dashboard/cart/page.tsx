76d84f9ee8456cb17b7ad3b5d1a1e747b3eaaabc
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-04-18 18:19:56 -0400

    Create products cart page

---
+++ src/app/dashboard/cart/page.tsx
@@ -0,0 +1,19 @@
+
+export const metadata = {
+  title: 'Cart',
+  description: 'Products Cart Page',
+}
+
+
+export default function CartPage() {
+  return (
+    <div>
+      <h1 className="text-5xl">Products Cart</h1>
+      <hr className="mb-2"/>
+
+      <div className="flex flex-col sm:flex-row gap-2 w-full">
+
+      </div>
+    </div>
+  );
+}
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,6 +1,7 @@
 import { cookies } from 'next/headers';
 import React from 'react'
 import { CiChat1, CiMenuBurger, CiSearch, CiShoppingBasket } from 'react-icons/ci'
+import Link from 'next/link';
 
 
 const getTotalCount = (cart: { [id: string]: number }):number => {
@@ -44,12 +45,14 @@ export const TopMenu = () => {
           <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
             <CiChat1 size={25} />
           </button>
-          <button className="p-2 flex items-center justify-center h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
+          <Link
+            href="/dashboard/cart"
+            className="p-2 flex items-center justify-center h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
             {
               ( totalItems > 0 ) && (<span className='text-sm mr-2 text-blue-800 font-bold'>{ totalItems }</span>)
             }
             <CiShoppingBasket size={25} />
-          </button>
+          </Link>
 
         </div>
       </div>
