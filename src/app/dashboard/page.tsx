bccf4d640cb68caf1944ca524d07b75900f274da
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-06-24 20:27:58 -0400

    Fix issue

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,8 +2,8 @@ import { WidgetItem } from "@/components";
 
 export default function DashboardPage() {
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-      <WidgetItem />
+    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {/* <WidgetItem /> */}
     </div>
   );
 }
